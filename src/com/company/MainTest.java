230dfb59169af858d98417390e8db7bda31b217e
gonzalogarcia <gjaubert@gmail.com>
2017-08-12 23:40:27 +0100

    Mega refactor: Board and GameEngine Class! Resolver for 1x1, 1x2, 2x1 and 2x2.

--- src/com/company/MainTest.java
+++ src/com/company/MainTest.java
@@ -1,74 +1,80 @@
 package com.company;
 
-import java.util.Objects;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * X always start
+ */
 class MainTest {
   Board board;
   GameEngine gameEngine;
 
   @Test
   void inAOneByOneBoardXWins() {
-    board = new Board(2, 2);
+    board = new Board(1, 1);
     gameEngine = new GameEngine(board);
     board.oneOne = Board.X_TOKEN;
-    String expected = GameEngine.X_WINS;
-    String actual = gameEngine.resolver.xWins(board);
 
+    String actual = gameEngine.resolve();
+
+    String expected = GameEngine.X_WINS;
     assertEquals(expected, actual);
   }
 
   @Test
   void inAOneByTwoBoardIsADraw() {
-    board = new Board(2, 2);
+    board = new Board(1, 2);
     gameEngine = new GameEngine(board);
-    String expected = "isDraw";
-    String board = "X"+"Y";
-    String actual = isDraw(board);
+    board.oneOne = Board.X_TOKEN;
+    board.oneTwo = Board.Y_TOKEN;
+
+    String actual = gameEngine.resolve();
+
+    String expected = GameEngine.IS_DRAW;
     assertEquals(expected, actual);
   }
 
-
   @Test
   void inATwoByOneBoardIsADraw() {
-    String expected = "isDraw";
-    String board = "X|Y";
-    String actual = isDraw(board);
+    board = new Board(2, 1);
+    gameEngine = new GameEngine(board);
+    board.oneOne = Board.X_TOKEN;
+    board.twoOne = Board.Y_TOKEN;
+
+    String actual = gameEngine.resolve();
+
+    String expected = GameEngine.IS_DRAW;
     assertEquals(expected, actual);
   }
 
   @Test
   void inATwoByOneBoardOp2IsADraw() {
-    String expected = "isDraw";
-    String board = "Y|X";
-    String actual = isDraw(board);
+    board = new Board(2, 1);
+    gameEngine = new GameEngine(board);
+    board.oneOne = Board.Y_TOKEN;
+    board.twoOne = Board.X_TOKEN;
+
+    String actual = gameEngine.resolve();
+
+    String expected = GameEngine.IS_DRAW;
     assertEquals(expected, actual);
   }
 
   @Test
   void inATwoByTwoBoardXWins() {
-    String expected = "x wins";
-    String board = "XY|YX";
-    String actual = xWins(board);
+    board = new Board(2, 2);
+    gameEngine = new GameEngine(board);
+    board.oneOne = Board.X_TOKEN;
+    board.oneTwo = Board.Y_TOKEN;
+    board.twoOne = Board.Y_TOKEN;
+    board.twoTwo = Board.X_TOKEN;
+
+    String actual = gameEngine.resolve();
+
+    String expected = GameEngine.X_WINS;
     assertEquals(expected, actual);
-  }
-
-  private String isDraw(String board) {
-    String result = null;
-    if (board.equals("XY") || board.equals("YX") || board.equals("X|Y") || board.equals("Y|X")) {
-      result = "isDraw";
-    }
-    return result;
-  }
-
-  private String xWins(Board board) {
-    String result = null;
-    if (board.oneOne.equals(Board.X_TOKEN) || board.equals("XY|YX") || board.equals("YX|XY") || board.equals("XY|XY") || board.equals("YX|YX")) {
-      result = "x wins";
-    }
-    return result;
   }
 
   class Board {
@@ -89,29 +95,91 @@ class MainTest {
 
   interface GameResolver {
     String xWins(Board board);
+
+    String isDraw(Board board);
   }
 
   class OneByOneResolver implements GameResolver {
     @Override public String xWins(Board board) {
       return board.oneOne.equals(Board.X_TOKEN) ? GameEngine.X_WINS : null;
     }
+
+    @Override public String isDraw(Board board) {
+      return null;
+    }
+  }
+
+  class OneByTwoResolver implements GameResolver {
+
+    @Override public String xWins(Board board) {
+      return null;
+    }
+
+    @Override public String isDraw(Board board) {
+      boolean op1 = board.oneOne.equals(Board.X_TOKEN) && board.oneTwo.equals(Board.Y_TOKEN);
+      boolean op2 = board.oneOne.equals(Board.Y_TOKEN) && board.oneTwo.equals(Board.X_TOKEN);
+      return (op1 || op2) ? GameEngine.IS_DRAW : null;
+    }
+  }
+
+  class TwoByOneResolver implements GameResolver {
+
+    @Override public String xWins(Board board) {
+      return null;
+    }
+
+    @Override public String isDraw(Board board) {
+      boolean op1 = board.oneOne.equals(Board.X_TOKEN) && board.twoOne.equals(Board.Y_TOKEN);
+      boolean op2 = board.oneOne.equals(Board.Y_TOKEN) && board.twoOne.equals(Board.X_TOKEN);
+      return (op1 || op2) ? GameEngine.IS_DRAW : null;
+    }
   }
 
   class TwoByTwoResolver implements GameResolver {
     @Override public String xWins(Board board) {
-      boolean op1 = Objects.equals(board.oneOne, "X") && Objects.equals(board.oneTwo, "Y")
-          && Objects.equals(board.twoOne, "Y") && Objects.equals(board.twoTwo, "X");
-
-      boolean op2 = Objects.equals(board.oneOne, "Y") && Objects.equals(board.oneTwo, "X")
-          && Objects.equals(board.twoOne, "X") && Objects.equals(board.twoTwo, "Y");
-
-      boolean op3 = Objects.equals(board.oneOne, "X") && Objects.equals(board.oneTwo, "Y")
-          && Objects.equals(board.twoOne, "X") && Objects.equals(board.twoTwo, "Y");
-
-      boolean op4 = Objects.equals(board.oneOne, "Y") && Objects.equals(board.oneTwo, "X")
-          && Objects.equals(board.twoOne, "Y") && Objects.equals(board.twoTwo, "X");
+      boolean op1 = board.oneOne.equals(Board.X_TOKEN)
+          && board.oneTwo.equals(Board.Y_TOKEN)
+          && board.twoOne.equals(Board.Y_TOKEN)
+          && board.twoTwo.equals(Board.X_TOKEN);
+      boolean op2 = board.oneOne.equals(Board.Y_TOKEN)
+          && board.oneTwo.equals(Board.X_TOKEN)
+          && board.twoOne.equals(Board.X_TOKEN)
+          && board.twoTwo.equals(Board.Y_TOKEN);
+      boolean op3 = board.oneOne.equals(Board.X_TOKEN)
+          && board.oneTwo.equals(Board.Y_TOKEN)
+          && board.twoOne.equals(Board.X_TOKEN)
+          && board.twoTwo.equals(Board.Y_TOKEN);
+      boolean op4 = board.oneOne.equals(Board.Y_TOKEN)
+          && board.oneTwo.equals(Board.X_TOKEN)
+          && board.twoOne.equals(Board.Y_TOKEN)
+          && board.twoTwo.equals(Board.X_TOKEN);
 
       return (op1 || op2 || op3 || op4) ? GameEngine.X_WINS : null;
+    }
+
+    @Override public String isDraw(Board board) {
+      return null;
+    }
+  }
+
+  class ResolverFactory {
+    public GameResolver getResolver(Board board) {
+      if (board == null) {
+        return null;
+      }
+      if (board.rows == 1 && board.columns == 1) {
+        return new OneByOneResolver();
+      }
+      if (board.rows == 1 && board.columns == 2) {
+        return new OneByTwoResolver();
+      }
+      if (board.rows == 2 && board.columns == 1) {
+        return new TwoByOneResolver();
+      }
+      if (board.rows == 2 && board.columns == 2) {
+        return new TwoByTwoResolver();
+      }
+      return null;
     }
   }
 
@@ -119,16 +187,23 @@ class MainTest {
     public static final String IS_DRAW = "isDraw";
     public static final String X_WINS = "x wins";
     public static final String Y_WINS = "y wins";
-    public  GameResolver resolver;
+    public GameResolver resolver;
     public Board board;
 
     public GameEngine(Board board) {
       this.board = board;
-      if (this.board.rows == 1 && this.board.columns == 1) {
-        resolver = new OneByOneResolver();
-      }
-      if (this.board.rows == 2 && this.board.columns == 2) {
-        resolver = new TwoByTwoResolver();
+      this.resolver = new ResolverFactory().getResolver(board);
+    }
+
+    public String resolve() {
+      String isDraw = resolver.isDraw(board);
+      String xWins = resolver.xWins(board);
+      if (isDraw == IS_DRAW) {
+        return IS_DRAW;
+      } else if (xWins == X_WINS) {
+        return X_WINS;
+      } else {
+        return Y_WINS;
       }
     }
   }
